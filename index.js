a5b8f5b05de923a07e5eb53b933d98a7748aaf87
Alisha Soriano <sorianoalisha@gmail.com>
2022-01-08 14:49:35 -0500

    Add attribution for both the photographer and Unsplash

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const body = document.body;
+const authorP = document.querySelector('#author');
 
 //access Unsplash API to get a random photo
 fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=experimental&client_id=FngrTy9r1X_CbbUaxsie0ll_C4W_xqux07X6JxzHLQc&')
@@ -6,4 +7,5 @@ fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=experi
     .then(data => {
         console.log(data)
         body.style.backgroundImage = `url(${data.urls.full})`; //set the background to the image
-    });
+        authorP.innerHTML = `By: ${data.user.name} on <a class='unsplash-attribution' href='https://unsplash.com/?utm_source=your_app_name&utm_medium=referral'>Unsplash</a>`;
+      });

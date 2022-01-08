d823c49e79a9f6096d975597eaba3f20486eb91b
Alisha Soriano <sorianoalisha@gmail.com>
2022-01-08 16:02:32 -0500

    Add a catch block to the fetch request

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=experi
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        body.style.backgroundImage = `url(${data.urls.full})`; //set the background to the image
-        authorP.innerHTML = `By: ${data.user.name} on <a class='unsplash-attribution' href='https://unsplash.com/?utm_source=your_app_name&utm_medium=referral'>Unsplash</a>`;
-      });
+        if(!data.errors) {
+          body.style.backgroundImage = `url(${data.urls.full})`; //set the background to the image
+          authorP.innerHTML = `By: ${data.user.name} on <a class='unsplash-attribution' href='https://unsplash.com/?utm_source=your_app_name&utm_medium=referral'>Unsplash</a>`;
+        }
+        else {
+          throw Error('There\'s been an error with the fetch request to the Unsplash API');
+        }
+    })
+    .catch(error => {
+      alert('A new image could not be retrieved at this time');
+      body.style.backgroundImage = `url(https://images.unsplash.com/photo-1581488066648-56a585ab6767?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfGFsbHx8fHx8fHx8fDE2NDE2NzUzMzU&ixlib=rb-1.2.1&q=85)`;
+    });

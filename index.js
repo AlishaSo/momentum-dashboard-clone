4683e03523587e07e2ef040e61949d93d4d69667
Alisha Soriano <sorianoalisha@gmail.com>
2022-01-11 15:27:47 -0500

    Add day's focus tasksection and styles

--- index.js
+++ index.js
@@ -3,49 +3,54 @@ const photographerP = document.querySelector('.photographer');
 const weatherDiv = document.querySelector('.weather');
 const timeEl = document.querySelector('.time');
 const quoteP = document.querySelector('#quote');
+const focusTaskDiv = document.querySelector('.focus-task-div');
+let taskIndex = 1;
+const focusTaskInput = document.querySelector('#focus-task');
+
+getCurrentTime();
 
 //Access Unsplash API to get a random photo
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=experimental')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    if(!data.errors) {
-      body.style.backgroundImage = `url(${data.urls.full})`; //set the background to the image
-      photographerP.innerHTML = `By: <a href='https://unsplash.com/@${data.user.username}' target='_blank'>${data.user.name}</a> on <a href='https://unsplash.com/?utm_source=your_app_name&utm_medium=referral' target='_blank'>Unsplash</a>`;
-    }
-    else {
-      throw Error('There\'s been an error with the fetch request to the Unsplash API');
-    }
-  })
-  .catch(() => {
-    alert('A new image could not be retrieved at this time');
-    body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1581488066648-56a585ab6767?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfGFsbHx8fHx8fHx8fDE2NDE2NzUzMzU&ixlib=rb-1.2.1&q=85)';
-    photographerP.innerHTML = `By: <a href='https://unsplash.com/@jrkorpa' target='_blank'>Jr Korpa</a> on <a href='https://unsplash.com/?utm_source=your_app_name&utm_medium=referral'>Unsplash</a>`;
-  });
+// fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=experimental')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data)
+//     if(!data.errors) {
+//       body.style.backgroundImage = `url(${data.urls.full})`; //set the background to the image
+//       photographerP.innerHTML = `By: <a href='https://unsplash.com/@${data.user.username}' target='_blank'>${data.user.name}</a> on <a href='https://unsplash.com/?utm_source=your_app_name&utm_medium=referral' target='_blank'>Unsplash</a>`;
+//     }
+//     else {
+//       throw Error('There\'s been an error with the fetch request to the Unsplash API');
+//     }
+//   })
+//   .catch(() => {
+//     alert('A new image could not be retrieved at this time');
+//     body.style.backgroundImage = 'url(https://images.unsplash.com/photo-1581488066648-56a585ab6767?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfGFsbHx8fHx8fHx8fDE2NDE2NzUzMzU&ixlib=rb-1.2.1&q=85)';
+//     photographerP.innerHTML = `By: <a href='https://unsplash.com/@jrkorpa' target='_blank'>Jr Korpa</a> on <a href='https://unsplash.com/?utm_source=your_app_name&utm_medium=referral'>Unsplash</a>`;
+//   });
 
 //Access Geolocation API to get user's coordinates
-navigator.geolocation.getCurrentPosition(position => {
-  //Access OpenWeather API to get the weather information
-  fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
-    .then(response => {
-      if (!response.ok) { //Check if fetch response is successful
-        throw Error("Weather data not available")
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data)
-      //Save the link to the icon image to a variable
-      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-      //Add the icon for the current weather to the weather div
-      weatherDiv.innerHTML = `
-        <img id='weather-icon' src='${iconUrl}'/>
-        <p class='weather-temp'>${Math.round(data.main.temp)}°</p>
-        <p class='weather-city'>${data.name}</p>
-      `;
-    })
-    .catch(error => console.error(error));
-});
+// navigator.geolocation.getCurrentPosition(position => {
+//   //Access OpenWeather API to get the weather information
+//   fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+//     .then(response => {
+//       if (!response.ok) { //Check if fetch response is successful
+//         throw Error("Weather data not available")
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(data)
+//       //Save the link to the icon image to a variable
+//       const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+//       //Add the icon for the current weather to the weather div
+//       weatherDiv.innerHTML = `
+//         <img id='weather-icon' src='${iconUrl}'/>
+//         <p class='weather-temp'>${Math.round(data.main.temp)}°</p>
+//         <p class='weather-city'>${data.name}</p>
+//       `;
+//     })
+//     .catch(error => console.error(error));
+// });
 
 function getCurrentTime() {
   const now = new Date();
@@ -77,3 +82,15 @@ fetch('https://api.quotable.io/random?maxLength=100')
       <br>
       - Sorry to Bother You (2018)`;
   });
+
+  focusTaskInput.addEventListener('keyup', e => {
+    if(e.key === 'Enter' || e.keyCode === 13) {
+      focusTaskDiv.innerHTML = `
+        <div class='focus-check'>
+          <h2 id='today-focus'>TODAY</h2>
+          <input type='checkbox' id='task-${taskIndex}' name='task-complete'/> <label for='task-${taskIndex}'>${focusTaskInput.value}</label>
+        </div>
+      `;
+    }
+    index++;
+  })
